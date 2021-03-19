c362d1abd4ea8784daa30706d9ea16e4ca009a29
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-03-19 21:22:42 +0100

    Add files via upload

---
+++ Tkinter Apps/calculator_app_1/calculations.py
@@ -0,0 +1,11 @@
+import main
+
+#Třída kalkulací
+def calc_connected():
+    print("\ncalculations.py has been connected to your file")
+    main.main_connected()
+    print("     calculations.py")
+
+class Calculate:
+    def operation(self):
+        return
